df1034885045c90b21eede0dc37682230b991242
Jonathan Zacsh <jzacsh@gmail.com>
2011-12-20 00:34:25 -0500

    utilizing libs new capability to return false on error with canvas initialization..

--- input/inc/behaviors.js
+++ input/inc/behaviors.js
@@ -63,7 +63,9 @@ jzacsh.behaviors.svgToCanvas = function (c) {
   if ('length' in $svg && $svg.length > 0 &&
       'length' in $canvas && $canvas.length > 0) {
     var mapper = svgToCanvas.mapToCanvas($svg[0], $canvas[0], config);
-    mapper.renderToCanvas();
+    if (mapper) {
+      mapper.renderToCanvas();
+    }
   }
 }
 
