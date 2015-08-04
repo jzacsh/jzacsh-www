a54df2b03ac560af1fa7b7b364c291db10191f84
Jonathan Zacsh <jzacsh@gmail.com>
2015-08-04 19:25:09 -0400

    more debugging, seems in right location

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -94,8 +94,11 @@ git checkout "$popdBranch"
 buildTarBall="$(mktemp -t "${mkTmpTemplate}.XXXXXXX.tgz")"
 npm install > /dev/null # too noisy
 npm run clean
+
+echo DEBUGGING: "version: $(npm run -s version)"
 echo DEBUGGING: $(git show -s --format='%cd' "$(npm run -s version)")
 exit 99
+
 npm run build || exit 89
 cd "$buildDir"
 tar -zcvf "$buildTarBall" ./*
